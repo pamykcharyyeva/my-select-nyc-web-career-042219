891cf56bfacde7a82acc4fc8aac087f861c15a27
Pamyk Charyyeva <pamyk.ch@gmail.com>
2019-04-14 21:06:18 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,9 +1,14 @@
 def my_select(collection)
-i=0 
-newArray = [ ]
-while i < collection.length do
-newArray[i]= yield collection[i]
-i=i+1
-end 
-newArray 
+i = 0
+selection = []
+
+ while i < collection.length do
+   if yield (collection[i])
+     selection << collection[i]
+   end
+
+   i += 1
+ end
+
+ selection
 end

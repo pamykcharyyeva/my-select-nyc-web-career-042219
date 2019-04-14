03b6628896eda62c1abfd805edd5f6e00793cb64
Pamyk Charyyeva <pamyk.ch@gmail.com>
2019-04-14 21:04:04 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,11 +1,9 @@
 def my_select(collection)
 i=0 
 newArray = [ ]
-while i< collection.length do
-if yield (collection[i]) == true 
-  newarray << collections[i]
-end 
-i+=1 
+while i < collection.length do
+newArray[i]= yield collection[i]
+i=i+1
 end 
 newArray 
 end
